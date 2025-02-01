f4345b5551f4a1641eb723e4d7b507f2afb48031
Levi Heßmann <levihsmn@gmail.com>
2025-02-02 00:46:16 +0100

    feat: Increase sidebar size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 // eslint-disable-next-line camelcase
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import React from 'react';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/custom/app-sidebar';
 import { Database } from '@/lib/types/mongo';
@@ -27,37 +28,18 @@ export default async function RootLayout({
 	children: React.ReactNode;
 }>) {
 	// TODO: Get real data
-	const databases: Database[] = [
-		{
-			name: 'test',
-			totalSize: 140960,
-			collections: [
-				{
-					name: 'test',
-					totalSize: 40960,
-				},
-			],
-		},
-		{
-			name: 'minecraft',
-			totalSize: 240960,
-			collections: [
-				{
-					name: 'players',
-					totalSize: 40960,
-				},
-				{
-					name: 'gamers',
-					totalSize: 4094434360,
-				},
-			],
-		},
-	];
+	const databases: Database[] = [];
 
 	return (
 		<html lang="en" className="w-full h-full">
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased w-full h-full`}>
-				<SidebarProvider>
+				<SidebarProvider
+					style={
+						{
+							'--sidebar-width': '350px',
+						} as React.CSSProperties
+					}
+				>
 					<AppSidebar databases={databases} totalSize={409443436} />
 					<SidebarInset>
 						<main>{children}</main>
