b01e68f2f37ff021dbf6d64077e8147e4ee77018
Levi Heßmann <levihsmn@gmail.com>
2025-02-01 23:56:20 +0100

    chore: Remove unused import

--- src/components/custom/app-sidebar.tsx
+++ src/components/custom/app-sidebar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ChevronRightIcon, DatabaseIcon, SearchIcon, SlashIcon, TableIcon } from 'lucide-react';
+import { ChevronRightIcon, DatabaseIcon, SearchIcon, TableIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
