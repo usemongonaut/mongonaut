1bbad19bebe051bc5b88d067f963ef0629afb9c3
Mathis Fullriede <mathis.fullriede@gmail.com>
2025-02-01 20:51:50 +0100

    first wip on mongo db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
+import { MongoClient } from 'mongodb';
+
+const mongoClient = new MongoClient('mongodb://localhost:27017');
+
+export default async function Home() {
+	await mongoClient.connect();
+	const admin = mongoClient.db().admin();
+	const result = await admin.listDatabases();
+	const serverInfo = await admin.serverInfo();
+	console.log(serverInfo);
+	console.log(result);
 	return <>mongonaut</>;
 }
