674fd16b42521594678a24ec88e994745f0a942d
Levi Heßmann <levihsmn@gmail.com>
2025-02-02 13:08:41 +0100

    feat: Add links in sidebar for table detail pages

--- src/components/custom/app-sidebar.tsx
+++ src/components/custom/app-sidebar.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import { useState } from 'react';
 import prettyBytes from 'next/dist/lib/pretty-bytes';
 import { Document } from 'mongodb';
+import { redirect } from 'next/navigation';
 import {
 	Sidebar,
 	SidebarContent,
@@ -152,7 +153,11 @@ export function CollapsibleDatabaseSidebarItem({
 				<CollapsibleContent>
 					<SidebarMenuSub>
 						{filteredCollections(database).map((collection, index) => (
-							<SidebarMenuButton key={index} className="data-[active=true]:bg-transparent">
+							<SidebarMenuButton
+								key={index}
+								onClick={() => redirect(`/${database.name}/${collection.name}`)}
+								className="data-[active=true]:bg-transparent cursor-pointer"
+							>
 								<TableIcon />
 								{collection.name}
 								<div className="ml-auto text-muted-foreground text-xs flex gap-1">
