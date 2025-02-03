eec22dac64986d12c9e35ae5d0d6591975f81d8e
Levi Heßmann <levihsmn@gmail.com>
2025-02-03 19:22:42 +0100

    feat: Make every document display user-side

--- src/app/[database]/[collection]/page.tsx
+++ src/app/[database]/[collection]/page.tsx
@@ -18,6 +18,7 @@ import {
 } from '@/actions/databaseOperation';
 import { envBool } from '@/lib/env';
 import { Input } from '@/components/ui/input';
+import { ClientDocumentView } from '@/components/custom/client-document-view';
 
 type Props = {
 	params: Promise<{
@@ -68,25 +69,7 @@ const CollectionDetailPage: FC<Props> = async ({ params: params }) => {
 					{contentArray.map((doc, index) => {
 						const plain = JSON.stringify(doc);
 
-						return (
-							<div className="border rounded-lg relative overflow-hidden" key={index}>
-								<ClientJsonEditor
-									className="w-full h-full overflow-scroll"
-									data={JSON.parse(plain)}
-									restrictAdd={isReadonly}
-									restrictDelete={isReadonly}
-									restrictEdit={isReadonly}
-									collapse={1}
-								/>
-
-								{isReadonly && (
-									<p className="text-xs font-semibold flex gap-2 text-primary-foreground uppercase bg-primary rounded-full py-0.5 px-2.5 absolute top-2.5 right-2.5">
-										<EyeIcon size={12} className="my-auto" />
-										Read-Only
-									</p>
-								)}
-							</div>
-						);
+						return <ClientDocumentView key={index} data={plain} isReadonly={isReadonly} />;
 					})}
 				</div>
 				<div>
--- src/components/custom/client-document-view.tsx
+++ src/components/custom/client-document-view.tsx
@@ -1,5 +1,26 @@
-import { WithId } from 'mongodb';
+'use client';
 
-export function ClientDocumentView({ data }: { data: WithId<Document>[] }) {
-	return <div></div>;
+import { EyeIcon } from 'lucide-react';
+import { ClientJsonEditor } from '@/components/custom/client-json-editor';
+
+export function ClientDocumentView({ data, isReadonly }: { data: string; isReadonly: boolean }) {
+	return (
+		<div className="border rounded-lg relative overflow-hidden">
+			<ClientJsonEditor
+				className="w-full h-full overflow-scroll"
+				data={JSON.parse(data)}
+				restrictAdd={isReadonly}
+				restrictDelete={isReadonly}
+				restrictEdit={isReadonly}
+				collapse={1}
+			/>
+
+			{isReadonly && (
+				<p className="text-xs font-semibold flex gap-2 text-primary-foreground uppercase bg-primary rounded-full py-0.5 px-2.5 absolute top-2.5 right-2.5">
+					<EyeIcon size={12} className="my-auto" />
+					Read-Only
+				</p>
+			)}
+		</div>
+	);
 }
