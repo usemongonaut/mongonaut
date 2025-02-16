759bf417ebeb5c6e6373f1f16c3455aec1912d9e
Mathis Fullriede <mathis.fullriede@gmail.com>
2025-02-16 23:05:17 +0100

    fix: fix nested key-value search

--- src/components/custom/searchbar.tsx
+++ src/components/custom/searchbar.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { FilterIcon } from 'lucide-react';
-import { useState } from 'react';
-import { redirect } from 'next/navigation';
+import { FormEvent, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { Input } from '@/components/ui/input';
 
 interface Props {
@@ -11,27 +11,37 @@ interface Props {
 }
 
 export default function Searchbar(props: Props) {
+	const router = useRouter();
 	const [key, setKey] = useState<string | undefined>(props.defaultKey);
 	const [searchValue, setSearchValue] = useState<string | undefined>(props.defaultValue);
 
-	function search() {
-		const currentHrefWithoutQuery = window.location.href.split('?')[0];
-		redirect(`${currentHrefWithoutQuery}?key=${key}&value=${searchValue}`);
+	function handleSubmit(e: FormEvent) {
+		e.preventDefault();
+
+		const baseUrl = window.location.pathname;
+
+		const searchParams = new URLSearchParams();
+		if (key) searchParams.set('key', key);
+		if (searchValue) searchParams.set('value', searchValue);
+
+		const newUrl = `${baseUrl}?${searchParams.toString()}`;
+
+		router.push(newUrl);
 	}
 
 	return (
-		<form onSubmit={search} className="grid grid-cols-3 gap-2">
+		<form onSubmit={handleSubmit} className="grid grid-cols-3 gap-2">
 			<div className="flex gap-4">
 				<FilterIcon className="text-muted-foreground my-auto" />
-				<Input placeholder="Key" value={key} onChange={e => setKey(e.target.value)} />
+				<Input placeholder="Key" value={key || ''} onChange={e => setKey(e.target.value)} />
 			</div>
 			<Input
 				placeholder="Value"
-				value={searchValue}
+				value={searchValue || ''}
 				onChange={e => setSearchValue(e.target.value)}
 				className="col-span-2"
 			/>
-			<Input type="submit" className="hidden" />
+			<button type="submit" className="hidden" />
 		</form>
 	);
 }
