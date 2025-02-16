6ceba5f8ec72278359adb89befb2348ed4715a3f
Levi Heßmann <levihsmn@gmail.com>
2025-02-16 23:27:54 +0100

    feat: Change color of skeletons

--- src/app/[database]/[collection]/loading.tsx
+++ src/app/[database]/[collection]/loading.tsx
@@ -5,19 +5,19 @@ export default function Loading() {
 	return (
 		<AppContainer>
 			<div>
-				<Skeleton className="bg-muted w-52 h-6 rounded-lg" />
+				<Skeleton className="bg-card w-52 h-6 rounded-lg" />
 			</div>
 			<div className="w-full h-full grid lg:grid-cols-3 gap-4">
 				<div className="flex flex-col gap-4 lg:col-span-2 w-full h-full">
 					<div>
-						<Skeleton className="bg-muted w-full h-10 rounded-lg" />
+						<Skeleton className="bg-card w-full h-10 rounded-lg" />
 					</div>
-					<Skeleton className="bg-muted w-full rounded-lg h-52" />
-					<Skeleton className="bg-muted w-full rounded-lg h-52" />
-					<Skeleton className="bg-muted w-full rounded-lg h-52" />
+					<Skeleton className="bg-card w-full rounded-lg h-52" />
+					<Skeleton className="bg-card w-full rounded-lg h-52" />
+					<Skeleton className="bg-card w-full rounded-lg h-52" />
 				</div>
 				<div>
-					<Skeleton className="bg-muted w-full rounded-lg h-40" />
+					<Skeleton className="bg-card w-full rounded-lg h-40" />
 				</div>
 			</div>
 		</AppContainer>
